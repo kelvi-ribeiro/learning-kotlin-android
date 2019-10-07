815b8ce519c98128c60f82691d31bd0ff183e4c1
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-10-07 00:47:07 -0300

    Aula 26. Controle de Fluxo com For in. Usando Range com number in number..otherNumber e number in n downTo otherNumber

---
+++ src/flowFor.kt
@@ -0,0 +1,22 @@
+fun main(){
+    // for - loop
+    // loop infinito
+    val nome = "Kelvi Ribeiro"
+    for (c in nome){
+        //print(c)
+    }
+
+    for(i in 0..100 step 5){
+        // print("$i ")
+    }
+
+    for(i in 0..100 step 5){
+        if(i === 80){
+            //print("$i ")
+        }
+    }
+
+    for(j in 100 downTo  0){
+        print("$j ")
+    }
+}
