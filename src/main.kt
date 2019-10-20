64bc0eb353fd704d0cd4e360a57a7ecb53b5a43a
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-10-20 17:17:17 -0300

    Aula 37. Exemplos de Uso

--- src/main.kt
+++ src/main.kt
@@ -1,22 +1,9 @@
-fun media(vararg  notas:Float){
-    if(notas.isNotEmpty()){
-        var soma = 0f
-        for(nota in notas){
-            soma += nota
-        }
-        println("A média é ${soma / notas.size}")
-    }
-}
-
-fun <T> media(vararg valores: T){
-    for(valor in valores){
-        println(valor)
-    }
-}
-
-
 fun main() {
-    //media(9f,9f)
-    //media(8f,7f, 5f, 8f)
-    media(8,"Teste", false, 8f)
+    val str = "lorem ipsum"
+  str.capitalize()
+  str.decapitalize()
+  str.contains("bla")
+  str.startsWith("l")
+  str.startsWith("L")
+  arrayOf(1,2,34,5,5f,"aa")
 }
