2ea281f803173426de52888c7b4835501e97739f
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-10-21 00:08:45 -0300

    Aula 38. Exercício - Inversão de Strings

---
+++ src/exercises/ReverseString.kt
@@ -0,0 +1,10 @@
+package exercises
+
+fun reverseUsingSB(string: String): String {
+  return "TODO"
+}
+
+fun reverseUsingLoop(string: String): String {
+  return "TODO"
+}
+
---
+++ src/exercises/ReverseStringTest.kt
@@ -0,0 +1,15 @@
+package exercises
+
+import org.junit.Assert
+import org.junit.Test
+
+class ReverseStringTest {
+  @Test fun reverseUsingSB(){
+    Assert.assertEquals("bs niltok", reverseUsingSB("kotlin sb"))
+  }
+
+  @Test fun reverseUsingLoop(){
+    Assert.assertEquals("", reverseUsingLoop("pool niltok"))
+  }
+
+}
