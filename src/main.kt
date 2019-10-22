d14c319c2304354fe2daed5398fc81e6205348b8
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-10-22 14:54:36 -0300

    Aula 46. Apredendo sobre construtores padrão e específicos

--- src/main.kt
+++ src/main.kt
@@ -1,18 +1,24 @@
 class Pessoa1
 
 
-
 class Pessoa2(var nome: String, val anoNascimento: Int)
 
-class Pessoa3(private var nome: String, private val anoNascimento: Int) {
+class Pessoa3(var nome: String) {
 
-    fun saudacao(){
-      println("Olá, meu nome é $nome e sou do ano de $anoNascimento")
-    }
+  var anoNascimento: Int? = null
+
+  constructor(nome: String, anoNascimento: Int) : this(nome) {
+    this.anoNascimento = anoNascimento
+  }
+
+  fun saudacao() {
+    println("Olá, meu nome é $nome e sou do ano de $anoNascimento")
+  }
 }
 
 
 fun main() {
-      val kelvi:Pessoa3 = Pessoa3("Kelvi", 1997)
-      kelvi.()
+  val kelvi: Pessoa3 = Pessoa3("Kelvi", 1997)
+  val thais: Pessoa3 = Pessoa3("Thaís")
+  thais.saudacao()
 }
