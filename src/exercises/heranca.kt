c22169ea0f0ba75a66b3d65a7d1e0367562338b5
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-10-26 03:45:36 -0300

    Aula 53. Sobreescrita e Sobrecarga (Override e Overload)

--- src/exercises/heranca.kt
+++ src/exercises/heranca.kt
@@ -1,18 +1,32 @@
 package exercises
 
 /*
-    Para que uma classe seja herdada por outra, precisamos que a classe
+    Heraça: Para que uma classe seja herdada por outra, precisamos que a classe
     pai tenha a palavra reservada de open, além disso, na classe filha, precisamos
     chamar o construtor da classe pai passando os parâmetros
+    Overide: Para sobreescrever métodos no Kotlin, é necessário a palavra reservada open
+
+    Overload: É quando se tem um método com assinatura diferente do pai
+    Assinatura: É a quantidade de parâmetros e seus respectivos tipos de um método
  */
 
 open class Maquina(val marca: String){
-    fun minhaMarca(){
+    open fun minhaMarca(){
         println("Minha marca é $marca")
     }
 }
 
 class Computador(marca: String, val nucleos: Int) : Maquina(marca){
+    override fun minhaMarca() {
+        println("Estou reescrevendo minha $marca")
+    }
+    // Sobrecarga
+    fun overload(i: Int) = println("Overload 1")
+
+    fun overload(s: String) = println("Overload 1")
+
+    fun overload(i: Int, b:Boolean) = println("Overload 1")
+
     fun ligar(){
         println("Ligando")
     }
@@ -27,5 +41,8 @@ fun main() {
         minhaMarca()
         ligar()
         operacoes()
+        overload(10)
+        overload(10, true)
+        overload("Olá")
     }
 }
