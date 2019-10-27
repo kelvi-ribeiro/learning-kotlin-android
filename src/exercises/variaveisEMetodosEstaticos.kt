6a6360ce8be6b7017990eb3778191c393fc6dd9e
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-10-27 15:57:35 -0300

    Aula 55. Constatntes e Variáveis estáticas

---
+++ src/exercises/variaveisEMetodosEstaticos.kt
@@ -0,0 +1,35 @@
+package exercises
+
+/*
+    Companion object: Um objeto que não necessariamente tem um nome e serve para criar atributos,
+    métodos que irão fica num escopo num estático. Em outras palavras,
+    os recursos da classe que são declaradas dentro um companion object ficam
+    no escopo da classe e não de uma instância da classe, pode-se criar hieraraquias, mas o nome é necessário
+    para isso
+    Object: Tem os mesmo recursos de companion object, mas precisa ter um nome associado e
+    com ele, podemos fazer uma hieraraquia de objets
+
+ */
+
+class Constants private  constructor() /* Fechando um construtor, pois nesse caso, não precisa */ {
+    companion object {
+        val TABLE = "Pessoa"
+
+        fun teste() {
+            println("Sou um método estático...")
+        }
+    }
+    object VENDAS {
+        val TABLE = "Vendas"
+
+        object COLUNAS {
+            val ID = "Id"
+        }
+    }
+}
+
+fun main() {
+    println(Constants.TABLE)
+    Constants.teste()
+    println(Constants.VENDAS.COLUNAS.ID)
+}
