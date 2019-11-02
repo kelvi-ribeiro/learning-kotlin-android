3089abdbced4f27e8d5f4d8cf4533ea57e3ac983
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-11-02 10:24:47 -0300

    Aula 76. Removendo Contato - Business e Repository

--- projetos/agenda-java-swing/src/repository/ContactRepository.kt
+++ projetos/agenda-java-swing/src/repository/ContactRepository.kt
@@ -12,6 +12,13 @@ class ContactRepository {
         }
 
         fun delete(contact: ContactEntity) {
+            // contactList.remove(contact)
+            for (item in contactList.withIndex()) {
+                if (item.value.name === contact.name && item.value.phone === contact.phone) {
+                    contactList.removeAt(item.index)
+                    break
+                }
+            }
 
         }
 
