31a826d4b08b090ef8308207811234f5f8b36b06
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-11-03 21:03:11 -0300

    Aula 106. Lambdas

--- exercicios-basicos/src/moreFunctions/highOrder.kt
+++ exercicios-basicos/src/moreFunctions/highOrder.kt
@@ -1,9 +1,12 @@
 package moreFunctions
 
 /*
-    High Order Functions: São funções que podem receber outras funções como parâmetro,
-    abaixo tem o exemplo de como isso funciona
-*/
+    Uma expressão lambda é uma maneira concisa de declarar uma função ou subrotina. Num mundo orientado a objetos, é bastante similar a um objeto que tem um método só.
+
+    Existem duas vantagens principais de se usar uma expressão lambda ao invés de uma função ou método definida separadamante:
+
+    1. Simplicidade. Para que dar um nome a algo que só vai ser usado uma vez mesmo?
+    2. Escopo léxico. Funções lambda podem automaticamente usar todas as variáveis do escopo atual.*/
 
 
 fun operator(x: Int, y: Int, op: (Int, Int) -> Int): Int {
@@ -32,9 +35,22 @@ fun main() {
     strlist.paraCada { println(it) }
 
     val map = mapOf(Pair("Paris", "França"))
-    map.values.paraCada{ println(it)}
+    map.values.paraCada { println(it) }
 
     operator(1, 2, ::sum)
 
     operator(1, 2, ::multiply)
+
+    operator(1, 2, { x: Int, y: Int -> x - y })
+
+    val l1 = { x: Int, y: Int -> x - y }
+
+    operator(1, 2, l1)
+
+
+    val l2: (Int, Int) -> Int = { u, i -> u + i }
+
+    operator(1, 2, l2)
+
+
 }
