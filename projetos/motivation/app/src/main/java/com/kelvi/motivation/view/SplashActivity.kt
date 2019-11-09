274562bbb13ef69cee71d8f2d8fb9ed70a0aeadc
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-11-09 18:59:26 -0300

    Aula 123. Utilizando o ConstraintLayout - Parte 1

--- projetos/motivation/app/src/main/java/com/kelvi/motivation/view/SplashActivity.kt
+++ projetos/motivation/app/src/main/java/com/kelvi/motivation/view/SplashActivity.kt
@@ -1,14 +1,13 @@
 package com.kelvi.motivation.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.kelvi.motivation.R
 import com.kelvi.motivation.util.MotivationConstants
 import com.kelvi.motivation.util.SecurityPreferences
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity(), View.OnClickListener {
